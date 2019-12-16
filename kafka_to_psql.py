2e87168abc2016fa8e47f05204aecab6bef37efd
tuomas@humanyze.com <Tuomas Jaanu>
2019-12-16 23:14:33 +0200

    Add comments

--- kafka_to_psql.py
+++ kafka_to_psql.py
@@ -10,14 +10,18 @@ def message_to_value_lists(message):
     :param message: Message value as it arrives through Kafka
     :return: list of value lists to be inserted to PostgreSQL
     """
+
+    # pull out common values for all rows
     hostname = message['host']
     metrics = message['metrics']
     timestamp = message['timestamp']
 
     values = []
 
+    # traverse message structure (double dictionary)
     for metric_key, metric_values in metrics.items():
         for submetric_key, value in metric_values.items():
+            # emit values for each row to be inserted to SQL
             values.append([timestamp, hostname, metric_key, submetric_key, value])
 
     return values
