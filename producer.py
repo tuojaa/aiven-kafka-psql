f02b8c1770abf8a4b8b3bdfca780ab4b5a5caeb8
tuomas@humanyze.com <Tuomas Jaanu>
2019-12-16 15:21:35 +0200

    Fix quotes

--- producer.py
+++ producer.py
@@ -3,13 +3,12 @@ from metrics import get_cpu_usage
 
 class MetricProducer(object):
 
-
     def collect_metrics(self):
-        '''
+        """
         Collect metrics. Each metric will be a dictionary, containing submetrics as key-value pairs
 
         :return: Dictionary of dictionaries
-        '''
+        """
         return {
             'cpu': get_cpu_usage()
         }
